ad738211bc747567b77479680953c7c942d88b33
Davazano <davideoguche@gmail.com>
2023-02-03 10:23:00 +0000

    Added HEADER_SIZE to client Added new_msg variable to track state of incomming message Print message from server with buffering and streaming data taken into consideration

--- client.py
+++ client.py
@@ -1,16 +1,33 @@
 import socket
 
+# Constants
+HEADER_SIZE = 20
+
 # instantiate socket, socket.AF_INET for ipV4, SOCK_STREAM for TCP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect to server's socket (using Ip address and port number)
 sock.connect((socket.gethostname(), 1233))
 
-full_msg = ''
 while True:
-    # store buffered message
-    msg = sock.recv(8)
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode("utf-8")
 
-print(full_msg)
+    full_msg = ''
+    new_msg = True
+    while True:
+        # store buffered message
+        msg = sock.recv(24)
+        if new_msg:
+            print(f"new message length: {msg[:HEADER_SIZE]}")
+            msglen = int(msg[:HEADER_SIZE])
+            new_msg = False
+        
+        full_msg += msg.decode("utf-8")
+
+        # Check length of message
+        if len(full_msg)-HEADER_SIZE == msglen:
+            print("full msg recvd")
+            # print message without header
+            print(full_msg[HEADER_SIZE:])
+            new_msg = True
+            full_msg = ''
+
+    print(full_msg)
