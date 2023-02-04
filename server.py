077beeba65015bee89f2561c2e3c52d7967c5f22
Davazano <davideoguche@gmail.com>
2023-02-04 10:33:31 +0000

    Remove sockets that are in exception_sockets list

--- server.py
+++ server.py
@@ -72,4 +72,6 @@ while True:
                 if client_socket != notified_socket:
                     client_socket.send(user["header"] + user["data"] + message['header'] + message['data'])
     
-    
+    for notified_socket in exception_sockets:
+        sockets_list.remove(notified_socket)
+        del clients[notified_socket]
