42fe092396dd8e991f6578d55ab7ab8b0c9b00a9
Davazano <davideoguche@gmail.com>
2023-02-04 08:01:27 +0000

    Refactored server and added receive_message method

--- server.py
+++ server.py
@@ -1,45 +1,35 @@
 import socket
-import keyboard
-import time
-import pickle
-
-# code for new branch
+import select
 
 # Constants
-HEADER_SIZE = 20
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 1234
 
 # instantiate socket, socket.AF_INET for ipV4, SOCK_STREAM for TCP
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# SET SOCKET OPTION LEVEL's SOCKET OPTION REUSE ADDRESS TO TRUE. This allows reconnection
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # bind socket. Note you can use any free port. Sockets are endpoints that sends and receives data. Sockets are created using Ip address and port number
-sock.bind((socket.gethostname(), 1233))
+server_socket.bind((IP, PORT))
 # listen (for incomming connections) using the socket. Queue of 5
-sock.listen(5)
+server_socket.listen()
 
-run_program = True
-# continue to run until condition changes
-while run_program:
-    print("waiting for client")
-    # print key pressed
-    print(keyboard.read_key())
+# list of clients
+sockets_list = [server_socket]
+# clients dictionary, key => client's socket, value => data from user
 
-    # check if keypressed is 'Q'
-    if keyboard.read_key() == "Q":
-        # change state run_program to False
-        run_program = False
-        # print message showing that chat app is closed
-        print("Closed Chat App")
-        break
-    
-    # accept connections, store clientsocket, and ip address
-    clientsocket, address = sock.accept()
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
 
-    print(f"Established connection with {address}!")
+        # if we didn't get any data. Probably due to client closing connection
+        if not len(message_header):
+            return False
+        
+        message_length = int(message_header.decode("utf-8").strip())
+        return {"header": message_header, "data": client_socket.recv(message_length)}
 
-    # Form a message with the fixed length header
-    d = {1: "Hey",  2: "There"}
-    msg = pickle.dumps(d)
+    except:
+        return False
 
-    msg = bytes(f'{len(msg):<{HEADER_SIZE}}', "utf-8") + msg
-
-    # send information to the client socket
-    clientsocket.send(msg)
