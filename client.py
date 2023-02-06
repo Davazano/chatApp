9b3b8b76e3246bea19c2f8bd9161ecc73685c070
Davazano <davideoguche@gmail.com>
2023-02-06 12:26:29 +0000

    set and send username set recv method to not blocking

--- client.py
+++ client.py
@@ -11,3 +11,14 @@ my_username = input("Username: ")
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client_socket.connect((IP, PORT))
+
+# set recv method to not blocking
+client_socket.setblocking(False)
+
+# set username
+username = my_username.encode("utf-8")
+# set username header
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode("utf-8")
+# send username
+client_socket.send(username_header + username)
+
