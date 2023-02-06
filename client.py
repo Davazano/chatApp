d667ea5b85c6ed60da8374f98a084f16f9623783
Davazano <davideoguche@gmail.com>
2023-02-06 12:19:49 +0000

    Created socket object in client.py

--- client.py
+++ client.py
@@ -3,3 +3,11 @@ import select
 import errno
 
 HEADER_LENGTH = 10
+
+IP = "127.0.0.1"
+PORT = 1234
+my_username = input("Username: ")
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client_socket.connect((IP, PORT))
